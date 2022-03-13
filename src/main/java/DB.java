93e6c71906bd7bc8982271d24f947ea8dfbe8f32
NiebAnupat <anupat.k64@rsu.ac.th>
2022-03-13 17:59:04 +0700

    First Commit

---
+++ src/main/java/DB.java
@@ -0,0 +1,52 @@
+import java.sql.*;
+public class DB {
+    private Connection con = null;
+    private Statement st = null;
+    private ResultSet rs = null;
+    private String url = null;
+    private String username = "";
+    private String password = "";
+    
+    public void connectDB()throws SQLException {
+        try{
+            con = DriverManager.getConnection(url,username,password);
+        }
+        catch(Exception e){
+            
+        }
+    }
+    
+    public void DisconnectDB()throws SQLException {
+        try{
+            con.close();
+        }
+        catch(Exception e){
+            System.out.println("Error");
+        }
+    }
+    
+    public boolean execute(String SQL)throws SQLException {
+        try{
+            connectDB();
+            st = con.createStatement();
+            st.execute(SQL);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+        finally{DisconnectDB();}
+    }
+    
+    public ResultSet getResultSet(String SQL)throws SQLException {
+        try{
+            connectDB();
+            st = con.createStatement();
+            rs = st.executeQuery(SQL);
+            return rs;
+        }
+        catch(Exception e){
+         return null;   
+        }
+    }
+}
